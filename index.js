6381723705dad0691d5d16cf6232c26cc7aa2628
Emmanuel <emakaau5@gmail.com>
2022-08-18 01:16:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1=31;
+let num2=2;
+var multiply=62;
+
+let random=3;
+let interger=true;
+
+// Write your code here 
+
+let mod=4;
+let num3=25;
+let num4=7;
+
+
+let max=20;
+(0,5,10,15,20); 
